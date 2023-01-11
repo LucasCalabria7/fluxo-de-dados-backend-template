476208f5d33f143f9f5fbd221c16d5b81cf819f5
Lucas Calabria <lucascalabria86@gmail.com>
2023-01-11 11:23:21 -0300

    Data validation

--- src/index.ts
+++ src/index.ts
@@ -21,42 +21,120 @@ app.get("/accounts", (req: Request, res: Response) => {
 })
 
 app.get("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
 
-    const result = accounts.find((account) => account.id === id) 
+        const result = accounts.find((account) => account.id === id)
 
-    res.status(200).send(result)
+        if (!result) {
+            res.status(404)
+            throw new Error("Account not found. Verify your id")
+        }
+
+        res.status(200).send(result)
+
+    } catch (erro) {
+        console.log(erro)
+
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+
+        res.send(erro.message)
+    }
 })
 
 app.delete("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
 
-    const accountIndex = accounts.findIndex((account) => account.id === id)
+        if (id[0] !== "a") {
+            res.status(400)
+            throw new Error("Invalid id, must begin with letter 'a' ")
+        }
 
-    if (accountIndex >= 0) {
-        accounts.splice(accountIndex, 1)
+        const accountIndex = accounts.findIndex((account) => account.id === id)
+
+        if (accountIndex >= 0) {
+            accounts.splice(accountIndex, 1)
+            res.status(200).send("Item deletado com sucesso")
+        } else {
+            res.status(404)
+            throw new Error("Invalid id, please check the information")
+        }
+    } catch (erro) {
+        console.log(erro)
+
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+
+        res.send(erro.message)
     }
-
-    res.status(200).send("Item deletado com sucesso")
 })
 
 app.put("/accounts/:id", (req: Request, res: Response) => {
-    const id = req.params.id
+    try {
+        const id = req.params.id
 
-    const newId = req.body.id as string | undefined
-    const newOwnerName = req.body.ownerName as string | undefined
-    const newBalance = req.body.balance as number | undefined
-    const newType = req.body.type as ACCOUNT_TYPE | undefined
+        const newId = req.body.id as string | undefined
+        const newOwnerName = req.body.ownerName as string | undefined
+        const newBalance = req.body.balance as number | undefined
+        const newType = req.body.type as ACCOUNT_TYPE | undefined
 
-    const account = accounts.find((account) => account.id === id) 
+        if(newOwnerName !== undefined) {
+            if(newOwnerName.length < 2) {
+                res.status(400)
+                throw new Error("Seu nome deve conter mais do que 2 caracteres")
+            }
+        }
 
-    if (account) {
-        account.id = newId || account.id
-        account.ownerName = newOwnerName || account.ownerName
-        account.type = newType || account.type
+        if(newId !== undefined) {
+            if(newId[0] !== 'a') {
+                res.status(400)
+                throw new Error("Id inválido, o mesmo deve começar com a letra 'a'.")
+            }
+        }
 
-        account.balance = isNaN(newBalance) ? account.balance : newBalance
+        if(newBalance !== undefined) {
+            if(newBalance < 0) {
+                res.status(404)
+                throw new Error("Nova conta não pode ser negativa")
+            }
+            if(typeof newBalance !== "number") {
+                res.status(404)
+                throw new Error("Nova conta precisa ser um número")
+            }
+        }
+
+        if(newType !== undefined) {
+            if(newType !== "Ouro" && newType !== "Platina" && newType !== "Black") {
+                res.status(400)
+                throw new Error("Invalid category, please insert a valid one.")
+            }
+        }
+
+        const account = accounts.find((account) => account.id === id)
+
+        if (account) {
+            account.id = newId || account.id
+            account.ownerName = newOwnerName || account.ownerName
+            account.type = newType || account.type
+
+            account.balance = isNaN(newBalance) ? account.balance : newBalance
+            res.status(200).send("Atualização realizada com sucesso")
+        } else {
+            res.status(400)
+            throw new Error("Id não encontrada, favor inserir ID válida")
+        }
+
+    } catch (erro) {
+        console.log(erro)
+
+        if (res.statusCode === 200) {
+            res.status(500)
+        }
+
+        res.send(erro.message)
     }
-
-    res.status(200).send("Atualização realizada com sucesso")
 })
